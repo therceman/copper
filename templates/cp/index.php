0fd0eb11ee71eb9ac57b8a2bf7a78e55243da65b
rceman <rceman.pentest@gmail.com>
2021-01-18 17:56:06 +0200

    CP (links for seed & migrate)

--- templates/cp/index.php
+++ templates/cp/index.php
@@ -1,12 +1,26 @@
 <?php /** @var \Copper\Component\Templating\ViewHandler $view */
 
-use Copper\Component\CP\CPController; ?>
+use Copper\Component\CP\CPController;
+
+$migrate_url = $view->path(ROUTE_copper_cp_action, ['action' => CPController::ACTION_DB_MIGRATE]);
+$seed_url = $view->path(ROUTE_copper_cp_action, ['action' => CPController::ACTION_DB_SEED]);
+?>
 
 <?= $view->render('header') ?>
 
 <body class="markdown-body">
 <h4>Welcome to Copper <b>Control Panel</b>!</h4>
-<form method="post" action="<?=$view->path(ROUTE_post_copper_cp, ['action' => CPController::ACTION_LOGOUT])?>">
+
+<ul>
+    <li>
+        <a target="_blank" href="<?= $migrate_url ?>">Migrate</a>
+    </li>
+    <li>
+        <a target="_blank" href="<?= $seed_url ?>">Seed</a>
+    </li>
+</ul>
+
+<form method="post" action="<?= $view->path(ROUTE_copper_cp_action, ['action' => CPController::ACTION_LOGOUT]) ?>">
     <button type="submit">Logout</button>
 </form>
 </body>
